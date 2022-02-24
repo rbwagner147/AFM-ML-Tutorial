d7314ed909804eb71c23f830523c2b9cb1978b28
ArjunGupta100 <arjun.gupta.y02@gmail.com>
2022-02-24 10:53:17 -0500

    logistic regression

--- logistic_regression.py
+++ logistic_regression.py
@@ -1,10 +1,45 @@
+#Logistic Regression 
+
 import matplotlib.pyplot as plt
 import numpy as np
+import pandas as pd
 from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import classification_report, confusion_matrix
+import import_data.py
+from sklearn import metrics
 
-#model.fit ()
+dataset = pd.read_csv('')
+import_data_py(['Material used','AFM y_value'])
 
-#model.classes #for binary classification 
-#array([0,1])
+xtrain, xtest, ytrain, ytest = train_test_split(
+        x, y, test_size = 0.25, random_state = 0)
+
+
+y= np.arrange
+
+model = LogisticRegression(solver='liblinear', random_state=0)
+
+model.fit(x, y)
+
+model.classes_
+
+pred_data = model.predict_proba(x)
+
+model = LogisticRegression(solver='liblinear', C=10.0, random_state=0)
+model.fit(x, y)
+
+model.score(x, y)
+
+cnf_matrix = metrics.confusion_matrix(x, y)
+cnf_matrix
+
+print(classification_report(y, model.predict_proba(x)))
+
+plt.tight_layout()
+plt.title('AFM')
+plt.xlabel('Material used')
+plt.ylabel('Contact made or not')
+plt.legend()
+plt.show()
+
 
