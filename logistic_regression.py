df6996ebad197cec02705b3010a6c610b24bf51d
ArjunGupta100 <arjun.gupta.y02@gmail.com>
2022-03-03 10:17:09 -0500

    Updated Logistic Regression Code

--- logistic_regression.py
+++ logistic_regression.py
@@ -4,36 +4,36 @@ import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 from sklearn.linear_model import LogisticRegression
+from sklearn.model_selection import train_test_split
 from sklearn.metrics import classification_report, confusion_matrix
 import import_data.py
 from sklearn import metrics
 
 dataset = pd.read_csv('')
+dataset.head()
 import_data_py(['Material used','AFM y_value'])
 
-xtrain, xtest, ytrain, ytest = train_test_split(
-        x, y, test_size = 0.25, random_state = 0)
+x_train, x_test, y_train, y_test = train_test_split(dataset[['z distance']], y, test_size = 1, random_state = 0)
 
 
 y= np.arrange
 
 model = LogisticRegression(solver='liblinear', random_state=0)
 
-model.fit(x, y)
+model.fit(x_train, y_train)
 
 model.classes_
 
-pred_data = model.predict_proba(x)
+pred_data = model.predict(x_test)
 
-model = LogisticRegression(solver='liblinear', C=10.0, random_state=0)
-model.fit(x, y)
 
-model.score(x, y)
 
-cnf_matrix = metrics.confusion_matrix(x, y)
-cnf_matrix
+model.score(x_test, y_test)
 
-print(classification_report(y, model.predict_proba(x)))
+cnf_m = confusion_matrix(y, pred_data)
+report = classification_report(y, pred_data)
+
+print(classification_report(y, model.predict_proba(x_train)))
 
 plt.tight_layout()
 plt.title('AFM')
